dffa1da8d7358d8ecdf6fb21a9dc2a46f5495777
bhagya <bhagya@outlook.com>
2016-10-26 12:28:05 -0500

    added functionality for customer order

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -23,10 +23,38 @@ connection.query('SELECT * FROM PRODUCTS',function(err,results){
 });
 
 function getOrder(){
-	inquirer.prompt([{name:"product",message:"What product do you want?"},{name:"quantity",message:"Enter quantity: "}])
+	inquirer.prompt([{name:"productID",message:"What product do you want?"},{name:"quantity",message:"Enter quantity: "}])
 		.then(function(answer){
 		console.log(answer);
-		
+		processOrder(answer.productID,parseInt(answer.quantity));
 	});
+
+}
+
+function processOrder(productID,quantity){
+	//new available quantity after customer order
+	var availableQuantity,price;
+	connection.query('SELECT stockQuantity,price FROM products WHERE ?',{itemID:productID},function(err,results){
+		if(err) throw err;
+		availableQuantity = results[0].stockQuantity;
+		price = parseInt(results[0].price);
+				//check enough stock is available to place order
+		if(availableQuantity < quantity){
+			console.log("Insufficient quantity!")
+		}else{
+
+		var quantityRemaining = availableQuantity - quantity;
+		console.log('new quantity',quantityRemaining);
+			
+
+			connection.query('UPDATE products SET ? WHERE ?',[{stockQuantity:quantityRemaining},{itemID:productID}],function(err){
+				if(err) throw err;
+				console.log('updated products table');
+				console.log('The total cost of purchase is $', price*quantity);
+			});
+		}
+
+	});
+
 
 }
